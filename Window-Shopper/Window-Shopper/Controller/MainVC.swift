428595707621177f1745a971219ba59280e881a3
Jeffrey Umandap <jeffreyumandap@Jeff-Umandap-MacBook-Pro.local>
2018-12-02 19:33:57 +0800

    Input accessory views & IBDesignable

--- Window-Shopper/Window-Shopper/Controller/MainVC.swift
+++ Window-Shopper/Window-Shopper/Controller/MainVC.swift
@@ -9,10 +9,25 @@
 import UIKit
 
 class MainVC: UIViewController {
-
+    
+    @IBOutlet weak var wageText: CurrencyTxtField!
+    @IBOutlet weak var priceText: CurrencyTxtField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        let calcButton = UIButton(frame: CGRect(x: 0, y: 0, width: view.frame.size.width, height: 60))
+        calcButton.backgroundColor = #colorLiteral(red: 0.9411764741, green: 0.4980392158, blue: 0.3529411852, alpha: 1)
+        calcButton.setTitle("Calculate", for: .normal)
+        calcButton.setTitleColor(#colorLiteral(red: 1.0, green: 1.0, blue: 1.0, alpha: 1.0), for: .normal)
+        calcButton.addTarget(self, action: #selector(MainVC.calculate), for: .touchUpInside)
+        
+        wageText.inputAccessoryView = calcButton
+        priceText.inputAccessoryView = calcButton
+    }
+    
+    @objc func calculate() {
+        print("We got here")
     }
 
     override func didReceiveMemoryWarning() {
