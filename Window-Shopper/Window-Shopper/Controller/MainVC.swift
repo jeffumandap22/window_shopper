654c15313385ae66e7489d65f4b658881d737988
Jeffrey Umandap <jeffreyumandap@Jeff-Umandap-MacBook-Pro.local>
2018-12-02 18:56:39 +0800

    Textfield added and customized

--- Window-Shopper/Window-Shopper/Controller/MainVC.swift
+++ Window-Shopper/Window-Shopper/Controller/MainVC.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  MainVC.swift
 //  Window-Shopper
 //
 //  Created by Jeffrey Umandap on 02/12/2018.
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class MainVC: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
---
+++ Window-Shopper/Window-Shopper/View/CurrencyTxtField.swift
@@ -0,0 +1,31 @@
+//
+//  CurrencyTxtField.swift
+//  Window-Shopper
+//
+//  Created by Jeffrey Umandap on 02/12/2018.
+//  Copyright © 2018 Jeffrey Umandap. All rights reserved.
+//
+
+import UIKit
+
+class CurrencyTxtField: UITextField {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        backgroundColor = #colorLiteral(red: 1, green: 1, blue: 1, alpha: 0.2528627997)
+        layer.cornerRadius = 5.0
+        textAlignment = .center
+        
+        if placeholder == nil {
+            placeholder = " "
+        }
+        
+        if let p = placeholder {
+            let place = NSAttributedString(string: p, attributes: [.foregroundColor: #colorLiteral(red: 1.0, green: 1.0, blue: 1.0, alpha: 1.0)])
+            attributedPlaceholder = place
+            textColor = #colorLiteral(red: 1.0, green: 1.0, blue: 1.0, alpha: 1.0)
+        }
+        
+    }
+
+}
